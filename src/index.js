9d6456a9b12f692ca5a2ce8c26a0fc737227a998
sreelakshmig009 <sreelakshmig009@gmail.com>
2021-09-03 02:30:39 +0530

    Developer days

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React, { useEffect,useState } from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './App.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
